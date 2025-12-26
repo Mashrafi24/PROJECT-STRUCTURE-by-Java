dfa3d90299c88e6e4b047874df4641e4c8c98535
Mashrafi24 <mashrafirahman2024@gmail.com>
2025-12-26 09:09:01 +0600

    Add EmployeeDepartmentMap class with example data

---
+++ EmployeeDepartmentMap.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class EmployeeDepartmentMap {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> employees = new HashMap<>();
+
+        employees.put(1, "HR");
+        employees.put(2, "IT");
+        employees.put(3, "Finance");
+
+        System.out.println(employees);
+    }
+}
